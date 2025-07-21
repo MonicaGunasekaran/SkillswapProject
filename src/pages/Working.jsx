df5d9b832a1f750ff5753f8396284ff5f5f0385c
MonicaGunasekaran <gmonica2593@gmail.com>
2025-07-21 16:32:23 +0530

    testimonial and how crea mobile vieted

--- src/pages/Working.jsx
+++ src/pages/Working.jsx
@@ -1,43 +1,61 @@
-import React from 'react'
+import React from 'react';
 
 const Working = () => {
-  return (<>
-  <div class="h-100 pl-5 bg-blue-900 ">
-    <div class="flex">
-      <div class="grid ml-auto ">
-      <p class=" p-5 font-bold text-5xl">How It Works?</p>
-      <div class="ml-auto mr-5">
-      <ol class="text-right pt-1 text-xl text-white"><li>Create a Profile</li>
-      <li>Connect With Peers</li>
-      <li>Start Exchanging Knowledge</li></ol>
-      
-</div></div>
+  return (
+    <>
+      <div className="min-h-screen pl-5 bg-blue-900 text-white">
+        {/* Title and Steps */}
+        <div className="flex justify-end">
+          <div className="text-right mr-5">
+            <p className="p-5 font-bold text-5xl">How It Works?</p>
+            <ol className="pt-1 text-xl">
+              <li>Create a Profile</li>
+              <li>Connect With Peers</li>
+              <li>Start Exchanging Knowledge</li>
+            </ol>
+          </div>
+        </div>
 
-</div>
+        {/* Cards Section */}
+        <div className="flex flex-col md:flex-row justify-center items-center gap-6 mt-10 px-4">
+          {/* Card 1 */}
+          <div className="w-full md:w-1/3 bg-amber-500 rounded-xl p-5 text-center hover:bg-red-500 transition-all duration-300">
+            <p className="text-xl font-semibold mb-2">Create Profile</p>
+            <p className="text-blue-50 text-justify px-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+              consectetur ipsam vitae.
+            </p>
+            <button className="p-2 mt-4 w-32 hover:bg-blue-600 rounded-sm font-bold hover:text-amber-100 bg-blue-900 text-red-500">
+              Click Me
+            </button>
+          </div>
 
-<div class="flex flex-3 justify-between mt-3.5">
-<div class="h-50 hover:bg-red-500  rounded-xl lg:h-50 lg:mx-10 lg:text-xl pt-5 lg:w-90  bg-amber-500 w-50 ml-0 text-center">
-        <p>Create Profile</p>
-        <p class="px-3  text-blue-50 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex consectetur ipsam vitae </p>
-<button class="p-2 mt-2 w-30 hover:bg-blue-600 rounded-sm font-bold hover:text-amber-100 bg-blue-900 text-red-500">Click Me</button>
-      </div>
-      <div class="h-50  hover:bg-red-500 rounded-xl lg:h-50 lg:mx-10 pt-5 lg:w-90  bg-amber-500 w-50 ml-0 text-center">
-        <p>Create Profile</p>
-        <p class="px-3 text-blue-50 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex consectetur ipsam vitae </p>
-        <button class="p-2 mt-2 w-30 hover:bg-blue-600 rounded-sm font-bold hover:text-amber-100 bg-blue-900 text-red-500">Click Me</button>
-      </div>
-      <div class="h-50  hover:bg-red-500 rounded-xl lg:h-50 lg:mx-10 pt-5 lg:w-90  bg-amber-500 w-50 ml-0 text-center">
-        <p>Create Profile</p>
-        <p class="px-3 text-blue-50 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex consectetur ipsam vitae </p>
-        <button class="p-2 mt-2 w-30 hover:bg-blue-600 rounded-sm font-bold hover:text-amber-100 bg-blue-900 text-red-500">Click Me</button>
-      </div>
-      </div>
-      
-      </div>
+          {/* Card 2 */}
+          <div className="w-full md:w-1/3 bg-amber-500 rounded-xl p-5 text-center hover:bg-red-500 transition-all duration-300">
+            <p className="text-xl font-semibold mb-2">Connect Peers</p>
+            <p className="text-blue-50 text-justify px-3">
+              Build your learning network by connecting with others who share
+              your passion.
+            </p>
+            <button className="p-2 mt-4 w-32 hover:bg-blue-600 rounded-sm font-bold hover:text-amber-100 bg-blue-900 text-red-500">
+              Click Me
+            </button>
+          </div>
 
-</>
- 
-  )
-}
+          {/* Card 3 */}
+          <div className="w-full md:w-1/3 bg-amber-500 rounded-xl p-5 text-center hover:bg-red-500 transition-all duration-300">
+            <p className="text-xl font-semibold mb-2">Exchange Knowledge</p>
+            <p className="text-blue-50 text-justify px-3">
+              Start collaborating through live sessions, Q&A, and learning swaps.
+            </p>
+            <button className="p-2 mt-4 w-32 hover:bg-blue-600 rounded-sm font-bold hover:text-amber-100 bg-blue-900 text-red-500">
+              Click Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Working
+export default Working;
